6295cb2d30e352f2df1d4112585b55916c8ce2a8
iwinnter <145732126+iwinnter@users.noreply.github.com>
2025-12-26 16:21:37 +0800

    1 A6000PRO

--- scripts/finetune_2A40.sh
+++ scripts/finetune_2A40.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # MODEL_NAME="Qwen/Qwen2-VL-7B-Instruct"
 # MODEL_NAME="Qwen/Qwen2-VL-2B-Instruct"
 MODEL_NAME="Qwen/Qwen2.5-VL-3B-Instruct"
@@ -8,43 +7,38 @@ MODEL_NAME="Qwen/Qwen2.5-VL-3B-Instruct"
 
 export PYTHONPATH=src:$PYTHONPATH
 
-GLOBAL_BATCH_SIZE=8
-BATCH_PER_DEVICE=1
-NUM_DEVICES=2
+GLOBAL_BATCH_SIZE=32
+BATCH_PER_DEVICE=4
+NUM_DEVICES=1
 GRAD_ACCUM_STEPS=$((GLOBAL_BATCH_SIZE / (BATCH_PER_DEVICE * NUM_DEVICES)))
-
-# If you want to tune the `embed_token` with LoRA, You need to tune `lm_head` together
-# You should freeze the the merger also, becuase the merger is included in the vision_tower.
-
-# Please set the gradient_checkpointing to False when you are using LoRA with vision models.
 
 deepspeed src/train/train_sft.py \
     --use_liger True \
     --lora_enable True \
     --vision_lora True \
     --use_dora False \
-    --lora_namespan_exclude "['lm_head', 'embed_tokens','language_model', 'llm', 'model.model']" \
+    --lora_namespan_exclude "['lm_head', 'embed_tokens']" \
     --lora_rank 32 \
-    --lora_alpha 64 \
+    --lora_alpha 32 \
     --lora_dropout 0.05 \
     --num_lora_modules -1 \
     --deepspeed scripts/zero3.json \
     --model_id $MODEL_NAME \
-    --data_path /root/autodl-tmp/R2R/r2r_8point_sampled13177.json \
-    --image_folder /root/autodl-tmp/R2R/train \
+    --data_path /root/autodl-tmp/annotations_converted_v2.json \
+    --image_folder /root/autodl-tmp/train \
     --remove_unused_columns False \
     --freeze_vision_tower True \
     --freeze_llm True \
-    --freeze_merger True \
+    --freeze_merger False \
     --bf16 True \
     --fp16 False \
-    --disable_flash_attn2 False \
-    --output_dir output/lora_vision_test \
+    --disable_flash_attn2 True \
+    --output_dir output/lora_full_finetune \
     --num_train_epochs 1 \
     --per_device_train_batch_size $BATCH_PER_DEVICE \
     --gradient_accumulation_steps $GRAD_ACCUM_STEPS \
-    --image_min_pixels $((128 * 28 * 28)) \
-    --image_max_pixels $((256 * 28 * 28)) \
+    --image_min_pixels $((256 * 28 * 28)) \
+    --image_max_pixels $((960 * 28 * 28)) \
     --learning_rate 2e-4 \
     --weight_decay 0.1 \
     --warmup_ratio 0.03 \
@@ -55,6 +49,6 @@ deepspeed src/train/train_sft.py \
     --report_to tensorboard \
     --lazy_preprocess True \
     --save_strategy "steps" \
-    --save_steps 1000 \
+    --save_steps 2000 \
     --save_total_limit 10 \
     --dataloader_num_workers 4
